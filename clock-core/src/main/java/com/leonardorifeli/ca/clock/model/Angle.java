2cf6291398210fdfc491acfd09fcf105515013e7
Leonardo Rifeli <leonardorifeli@gmail.com>
2016-07-31 18:40:17 -0300

    Fixed angle class - added hashcode and toString method

--- clock-core/src/main/java/com/leonardorifeli/ca/clock/model/Angle.java
+++ clock-core/src/main/java/com/leonardorifeli/ca/clock/model/Angle.java
@@ -1,28 +1,36 @@
 package com.leonardorifeli.ca.clock;
 
-public class Angle
-{
+public class Angle {
     private final Double angle;
 
-    public Angle(Double angle)
-    {
+    public setAngle(Double angle) {
         this.angle = angle;
     }
 
-    public Double getAngle()
-    {
+    public Double getAngle() {
         return this.angle;
     }
 
     @Override
-    public boolean equals(Object alfa)
-    {
-        if(this == alfa) return true;
-        if(alfa == null || getClass() != alfa.getClass()) return false;
+    public boolean equals(Object alfa) {
+        if (this == alfa) return true;
+        if (alfa == null || getClass() != alfa.getClass()) return false;
 
         Angle angle = (Angle) alfa;
 
         return getAngle().equals(angle.getAngle());
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return getAngle().hashCode();
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Angle{angle="+this.angle+"}";
     }
 }
 
