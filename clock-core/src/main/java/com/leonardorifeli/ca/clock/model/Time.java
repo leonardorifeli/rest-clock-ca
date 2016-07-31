7289fd04e08bd0e171478f2102dacdc35cb45145
Leonardo Rifeli <leonardorifeli@gmail.com>
2016-07-31 18:41:27 -0300

    Created Time class and using Angle class

---
+++ clock-core/src/main/java/com/leonardorifeli/ca/clock/model/Time.java
@@ -0,0 +1,15 @@
+package com.leonardorifeli.contaaazul.clock.model;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Time
+{
+
+    private final Integer hour;
+    private final Integer minute;
+    private Angle angle;
+
+    
+
+}
