5a4aa1ab047dbb0a7b5c95a2a9f3cc1f48c81d50
Leonardo Rifeli <leonardorifeli@gmail.com>
2016-07-31 19:34:11 -0300

    Fixed time corrected line

--- clock-core/src/main/java/com/leonardorifeli/ca/clock/model/Time.java
+++ clock-core/src/main/java/com/leonardorifeli/ca/clock/model/Time.java
@@ -9,49 +9,51 @@ public class Time {
     private final Integer minute;
     private Angle angle;
 
-    public Time(final Integer hour, final Integer minute) throws IllegalArgumentException
-    {
-        if (hour == null || hour < 0) throw new IllegalArgumentException('Invalid value for hour.');
+    public Time(final Integer hour, final Integer minute) throws IllegalArgumentException {
+        if(hour == null || hour < 0) {
+            throw new IllegalArgumentException("Invalid value for hour");
+        }
 
         this.hour = (hour > 12) ? 12 - (24 - hour) : hour;
         this.minute = (minute == null || minute < 0) ? 0 : minute;
     }
 
-    public Integer getHour()
-    {
+    public Integer getHour() {
         return this.hour;
     }
 
-    public Integer getMinute()
-    {
+    public Integer getMinute() {
         return this.minute;
     }
 
     @Override
-    public boolean equals(Object o)
-    {
-        if(this == o) return true;
-        if(o == null || getClass() != o.getClass()) return false;
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
 
         Time time = (Time) o;
 
-        if(!getHour().equals(time.getHour())) return false;
+        if (!getHour().equals(time.getHour())) return false;
 
         return getMinute().equals(time.getMinute());
     }
 
     @Override
-    public boolean hashCode()
-    {
+    public int hashCode() {
         int result = getHour().hashCode();
+
         result = 31 * result + getMinute().hashCode();
+
         return result;
     }
 
     @Override
-    public String toString()
-    {
+    public String toString() {
+        return "Time{hour=" + this.hour + ", minutes=" + this.minute + "}";
+    }
 
+    public Angle getAngle() {
+        return this.angle;
     }
 
 }
