64990217572ef36287cff6cd9fc9fcadd5d8f1c0
Leonardo Rifeli <leonardorifeli@gmail.com>
2016-07-31 18:53:45 -0300

    Developing time class

--- clock-core/src/main/java/com/leonardorifeli/ca/clock/model/Time.java
+++ clock-core/src/main/java/com/leonardorifeli/ca/clock/model/Time.java
@@ -1,15 +1,43 @@
-package com.leonardorifeli.contaaazul.clock.model;
+package com.leonardorifeli.ca.clock.model;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Time
-{
+public class Time {
 
     private final Integer hour;
     private final Integer minute;
     private Angle angle;
 
-    
+    public Time(final Integer hour, final Integer minute) throws IllegalArgumentException
+    {
+        if (hour == null || hour < 0) throw new IllegalArgumentException('Invalid value for hour. Plase, check.');
+
+        this.hour = (hour > 12) ? 12 - (24 - hour) : hour;
+        this.minute = (minute == null || minute < 0) ? 0 : minute;
+    }
+
+    public Integer getHour()
+    {
+        return this.hour;
+    }
+
+    public Integer getMinute()
+    {
+        return this.minute;
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if(this == o) return true;
+        if(o == null || getClass() != o.getClass()) return false;
+
+        Time time = (Time) o;
+
+        if(!getHour().equals(time.getHour())) return false;
+
+        return getMinute().equals(time.getMinute());
+    }
 
 }
