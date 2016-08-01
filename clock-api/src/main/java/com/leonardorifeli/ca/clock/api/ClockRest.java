36f56405725b21ac47f3482400872766c7ee9158
Leonardo Rifeli <leonardorifeli@gmail.com>
2016-07-31 22:18:04 -0300

    Implemented the rest API response

--- clock-api/src/main/java/com/leonardorifeli/ca/clock/api/ClockRest.java
+++ clock-api/src/main/java/com/leonardorifeli/ca/clock/api/ClockRest.java
@@ -15,5 +15,40 @@ import javax.ws.rs.core.Response;
 @Path("/clock")
 @Produces("application/json")
 public class ClockRest {
-    
+
+    private static Logger LOG = LoggerFactory.getLogger(ClockRest.class);
+
+    @GET
+    @Path("/{hour}")
+    @Formatted
+    public Response getAngleForHour(final @PathParam("hour") Integer hour) {
+        return getAngle(hour, 0);
+    }
+
+    @GET
+    @Path("/{hour}/{minute}")
+    @Formatted
+    public Response getAngleForHourAndMinute(final @PathParam("hour") Integer hour, final @PathParam("minute") Integer minute) {
+        return getAngle(hour, minute);
+    }
+
+    private Response getAngle(final Integer hour, final Integer minute) {
+        if(hour < 0 || hour > 23) {
+            LOG.warn("Invalid value for hour: "+ hour);
+            return Response.status(400).build();
+        }
+
+        if(minute < 0 || minute > 59) {
+            LOG.warn("Invalid value for minutes: "+ minute);
+            return Response.status(400).build();
+        }
+
+        final Clock clock = new Clock(hour, minute);
+        final Angle angle = clock.getAngle();
+
+        LOG.debug("Angle for "+ clock + " is "+ angle.getAngle());
+
+        return Response.ok(angle).build();
+    }
+
 }
