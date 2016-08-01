070f88a3e0301ce7cb789da123312b01d0516d3e
Leonardo Rifeli <leonardorifeli@gmail.com>
2016-07-31 21:16:24 -0300

    Fixed renamed time variable to clock

--- clock-core/src/test/java/com/leonardorifeli/ca/clock/model/ClockTest.java
+++ clock-core/src/test/java/com/leonardorifeli/ca/clock/model/ClockTest.java
@@ -1,16 +1,16 @@
 package com.leonardorifeli.ca.clock.model;
 
-import com.leonardorifeli.ca.clock.model.Time;
+import com.leonardorifeli.ca.clock.model.Clock;
 import org.junit.Test;
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertTrue;
 
-public class TimeTest
+public class ClockTest
 {
     @Test
     public void testValidHour() {
         try {
-            Time time = new Time(13, 0);
+            Clock clock = new Clock(13, 0);
 
             assertTrue(true);
         } catch (IllegalArgumentException e) {
@@ -21,7 +21,7 @@ public class TimeTest
     @Test
     public void testValidMinute() {
         try {
-            Time time = new Time(13, 15);
+            Clock clock = new Clock(13, 15);
 
             assertTrue(true);
         } catch (IllegalArgumentException e) {
