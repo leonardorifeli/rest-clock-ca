0bcbfe10aeda05842ebe9099fce32f7c7eaeed6c
Leonardo Rifeli <leonardorifeli@gmail.com>
2016-07-31 23:09:11 -0300

    Implemented cache in calc angle

--- clock-api/src/main/java/com/leonardorifeli/ca/clock/api/ClockRest.java
+++ clock-api/src/main/java/com/leonardorifeli/ca/clock/api/ClockRest.java
@@ -2,6 +2,8 @@ package com.leonardorifeli.ca.clock.api;
 
 import com.leonardorifeli.ca.clock.model.Angle;
 import com.leonardorifeli.ca.clock.model.Clock;
+import com.leonardorifeli.ca.clock.util.DummyCache;
+import com.leonardorifeli.ca.clock.util.DummyCacheProvider;
 import org.jboss.resteasy.annotations.providers.jackson.Formatted;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,6 +19,7 @@ import javax.ws.rs.core.Response;
 public class ClockRest {
 
     private static Logger LOG = LoggerFactory.getLogger(ClockRest.class);
+    private static final DummyCache<String, Clock> DUMMY_CACHE = DummyCacheProvider.getCache();
 
     @GET
     @Path("/{hour}")
@@ -43,12 +46,28 @@ public class ClockRest {
             return Response.status(400).build();
         }
 
+        final String key = this.getCacheKey(hour, minute);
+        final Angle angle;
+
+        if(DUMMY_CACHE.contains(key)) {
+            angle = DUMMY_CACHE.get(key).getAngle();
+            return getResponseWithFormat(angle);
+        }
+
         final Clock clock = new Clock(hour, minute);
-        final Angle angle = clock.getAngle();
+        angle = clock.getAngle();
 
         LOG.debug("Angle for "+ clock + " is "+ angle.getAngle());
 
-        return Response.ok("{\"angle\":"+ String.format( "%.0f", angle.getAngle() ) +"}\n").build();
+        return getResponseWithFormat(angle.getAngle());
+    }
+
+    private Response getResponseWithFormat(final double angle) {
+        return Response.ok("{\"angle\":"+ String.format("%.0f", angle) +"}\n").build();
+    }
+
+    private final String getCacheKey(final Integer hour, final Integer minute) {
+        return hour+":"+minute;
     }
 
 }
