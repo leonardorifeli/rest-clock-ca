3c6e33cac0e97850c90a28489aaaf9a811237d7d
Leonardo Rifeli <leonardorifeli@gmail.com>
2017-03-11 01:57:21 -0300

    Replace spaces to tabs

--- clock-api/src/main/java/com/leonardorifeli/ca/clock/api/ClockRest.java
+++ clock-api/src/main/java/com/leonardorifeli/ca/clock/api/ClockRest.java
@@ -18,57 +18,57 @@ import javax.ws.rs.core.Response;
 @Produces("application/json")
 public class ClockRest {
 
-    private static Logger LOG = LoggerFactory.getLogger(ClockRest.class);
-    private static final DummyCache<String, Clock> DUMMY_CACHE = DummyCacheProvider.getCache();
+	private static Logger LOG = LoggerFactory.getLogger(ClockRest.class);
+	private static final DummyCache<String, Clock> DUMMY_CACHE = DummyCacheProvider.getCache();
 
-    @GET
-    @Path("/{hour}")
-    @Formatted
-    public Response getAngleForHour(final @PathParam("hour") Integer hour) {
-        return getAngle(hour, 0);
-    }
+	@GET
+	@Path("/{hour}")
+	@Formatted
+	public Response getAngleForHour(final @PathParam("hour") Integer hour) {
+		return getAngle(hour, 0);
+	}
 
-    @GET
-    @Path("/{hour}/{minute}")
-    @Formatted
-    public Response getAngleForHourAndMinute(final @PathParam("hour") Integer hour, final @PathParam("minute") Integer minute) {
-        return getAngle(hour, minute);
-    }
+	@GET
+	@Path("/{hour}/{minute}")
+	@Formatted
+	public Response getAngleForHourAndMinute(final @PathParam("hour") Integer hour, final @PathParam("minute") Integer minute) {
+		return getAngle(hour, minute);
+	}
 
-    private Response getAngle(final Integer hour, final Integer minute) {
-        if(hour < 0 || hour > 23) {
-            LOG.warn("Invalid value for hour: "+ hour);
-            return Response.status(400).build();
-        }
+	private Response getAngle(final Integer hour, final Integer minute) {
+		if(hour < 0 || hour > 23) {
+			LOG.warn("Invalid value for hour: "+ hour);
+			return Response.status(400).build();
+		}
 
-        if(minute < 0 || minute > 59) {
-            LOG.warn("Invalid value for minutes: "+ minute);
-            return Response.status(400).build();
-        }
+		if(minute < 0 || minute > 59) {
+			LOG.warn("Invalid value for minutes: "+ minute);
+			return Response.status(400).build();
+		}
 
-        final String key = this.getCacheKey(hour, minute);
-        final Angle angle;
+		final String key = this.getCacheKey(hour, minute);
+		final Angle angle;
 
-        if(DUMMY_CACHE.contains(key)) {
-            angle = DUMMY_CACHE.get(key).getAngle();
-            return getResponseWithFormat(angle.getAngle());
-        }
+		if(DUMMY_CACHE.contains(key)) {
+			angle = DUMMY_CACHE.get(key).getAngle();
+			return getResponseWithFormat(angle.getAngle());
+		}
 
-        final Clock clock = new Clock(hour, minute);
-        angle = clock.getAngle();
+		final Clock clock = new Clock(hour, minute);
+		angle = clock.getAngle();
 
-        LOG.debug("Angle for "+ clock + " is "+ angle.getAngle());
-        DUMMY_CACHE.put(key, clock);
+		LOG.debug("Angle for "+ clock + " is "+ angle.getAngle());
+		DUMMY_CACHE.put(key, clock);
 
-        return getResponseWithFormat(angle.getAngle());
-    }
+		return getResponseWithFormat(angle.getAngle());
+	}
 
-    private Response getResponseWithFormat(final double angle) {
-        return Response.ok("{\"angle\":"+ String.format("%.0f", angle) +"}\n").build();
-    }
+	private Response getResponseWithFormat(final double angle) {
+		return Response.ok("{\"angle\":"+ String.format("%.0f", angle) +"}\n").build();
+	}
 
-    private final String getCacheKey(final Integer hour, final Integer minute) {
-        return hour+":"+minute;
-    }
+	private final String getCacheKey(final Integer hour, final Integer minute) {
+		return hour+":"+minute;
+	}
 
 }
