8a26883a49d8e517a19961b7ab39c7f1d96e38b2
Leonardo Rifeli <leonardorifeli@gmail.com>
2017-03-11 01:51:52 -0300

    Replace spaces to tabs

--- clock-core/src/main/java/com/leonardorifeli/ca/clock/model/Clock.java
+++ clock-core/src/main/java/com/leonardorifeli/ca/clock/model/Clock.java
@@ -6,98 +6,98 @@ import sun.rmi.runtime.Log;
 
 public class Clock {
 
-    private Integer hour;
-    private Integer minute;
-    private Angle angle;
-    private static Logger LOG = LoggerFactory.getLogger(Clock.class);
+	private Integer hour;
+	private Integer minute;
+	private Angle angle;
+	private static Logger LOG = LoggerFactory.getLogger(Clock.class);
 
-    public Clock(final Integer hour, final Integer minute) throws IllegalArgumentException {
-        setHour(hour);
-        setMinute(minute);
-    }
+	public Clock(final Integer hour, final Integer minute) throws IllegalArgumentException {
+		setHour(hour);
+		setMinute(minute);
+	}
 
-    public void setHour(final Integer hour) throws IllegalArgumentException {
-        if(hour == null || hour < 0 || hour > 23)
-            throw new IllegalArgumentException("Invalid value for hour.");
+	public void setHour(final Integer hour) throws IllegalArgumentException {
+		if(hour == null || hour < 0 || hour > 23)
+			throw new IllegalArgumentException("Invalid value for hour.");
 
-        this.hour = getValidatedHour(hour);
-    }
+		this.hour = getValidatedHour(hour);
+	}
 
-    public void setMinute(final Integer minute) throws IllegalArgumentException {
-        if(minute == null || minute > 59 || minute < 0)
-            throw new IllegalArgumentException("Invalid value for minute.");
+	public void setMinute(final Integer minute) throws IllegalArgumentException {
+		if(minute == null || minute > 59 || minute < 0)
+			throw new IllegalArgumentException("Invalid value for minute.");
 
-        this.minute = (minute == null) ? 0 : minute;
-    }
+		this.minute = (minute == null) ? 0 : minute;
+	}
 
-    public Integer getHour() {
-        return hour;
-    }
+	public Integer getHour() {
+		return hour;
+	}
 
-    public Integer getMinute() {
-        return minute;
-    }
+	public Integer getMinute() {
+		return minute;
+	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o)
-            return true;
-            
-        if (o == null || getClass() != o.getClass())
-            return false;
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+			
+		if (o == null || getClass() != o.getClass())
+			return false;
 
-        Clock time = (Clock) o;
+		Clock time = (Clock) o;
 
-        if (!getHour().equals(time.getHour()))
-            return false;
+		if (!getHour().equals(time.getHour()))
+			return false;
 
-        return getMinute().equals(time.getMinute());
-    }
+		return getMinute().equals(time.getMinute());
+	}
 
-    @Override
-    public int hashCode() {
-        int result = getHour().hashCode();
+	@Override
+	public int hashCode() {
+		int result = getHour().hashCode();
 
-        result = 31 * result + getMinute().hashCode();
+		result = 31 * result + getMinute().hashCode();
 
-        return result;
-    }
+		return result;
+	}
 
-    @Override
-    public String toString() {
-        return "Time{hour=" + this.hour + ", minutes=" + this.minute + "}";
-    }
+	@Override
+	public String toString() {
+		return "Time{hour=" + this.hour + ", minutes=" + this.minute + "}";
+	}
 
-    public Angle getAngle() {
-        if(this.angle != null)
-            return this.angle;
+	public Angle getAngle() {
+		if(this.angle != null)
+			return this.angle;
 
-        this.angle = new Angle(this.calculateDegrees());
+		this.angle = new Angle(this.calculateDegrees());
 
-        return this.angle;
-    }
+		return this.angle;
+	}
 
-    private Integer getValidatedHour(Integer hour) {
-        return (hour > 12) ? 12 - (24 - hour) : hour;
-    }
+	private Integer getValidatedHour(Integer hour) {
+		return (hour > 12) ? 12 - (24 - hour) : hour;
+	}
 
-    private Integer getCalculatedHour(Integer hour) {
-        return 12 - (24 - hour);
-    }
+	private Integer getCalculatedHour(Integer hour) {
+		return 12 - (24 - hour);
+	}
 
-    private double calculateDegrees() {
-        final double degrees = Math.abs((60 * this.getHour() - 11 * this.getMinute()) / 2.0);
+	private double calculateDegrees() {
+		final double degrees = Math.abs((60 * this.getHour() - 11 * this.getMinute()) / 2.0);
 
-        LOG.debug("Calculating angle for "+this+". Degrees: "+degrees);
+		LOG.debug("Calculating angle for "+this+". Degrees: "+degrees);
 
-        return validateDegrees(degrees);
-    }
+		return validateDegrees(degrees);
+	}
 
-    private double validateDegrees(final double degrees) {
-        if(degrees <= 180)
-            return degrees;
+	private double validateDegrees(final double degrees) {
+		if(degrees <= 180)
+			return degrees;
 
-        return (360 - degrees);
-    }
+		return (360 - degrees);
+	}
 
 }
